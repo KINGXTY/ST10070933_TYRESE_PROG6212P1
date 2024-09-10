8a9967c40b024f99d73a7026c8c791f753fb8e5c
Tyrese Allan Hendricks <102513516+KINGXTY@users.noreply.github.com>
2024-09-10 16:11:24 +0200

    commit 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10070933_PROG6212P1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
